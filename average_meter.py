73a729c319ef9a003f1fe0d26f0669fcc59412ed
Toru Tamaki <tamaki@hiroshima-u.ac.jp>
2022-03-11 18:45:44 +0900

    add average meter

---
+++ average_meter.py
@@ -0,0 +1,25 @@
+import torch
+
+class AverageMeter(object):
+    """
+    Computes and stores the average and current value
+    Imported from https://github.com/pytorch/examples/blob/cedca7729fef11c91e28099a0e45d7e98d03b66d/imagenet/main.py#L363-L380
+    https://github.com/machine-perception-robotics-group/attention_branch_network/blob/ced1d97303792ac6d56442571d71bb0572b3efd8/utils/misc.py#L59
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        if type(val) == torch.Tensor:
+            val = val.item()
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
